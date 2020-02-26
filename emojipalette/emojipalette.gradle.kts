976b9e0cf83d188f56b5950c01befb041ea8787d
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-02-26 11:00:03 +0100

    emojipalette: Add plugin type

--- emojipalette/emojipalette.gradle.kts
+++ emojipalette/emojipalette.gradle.kts
@@ -25,7 +25,7 @@ import ProjectVersions.rlVersion
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.1"
+version = "0.0.2"
 
 project.extra["PluginName"] = "Emoji palette"
 project.extra["PluginDescription"] = "Shows a panel with all the emoji"
