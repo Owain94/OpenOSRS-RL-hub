e011dde7e8050a93b2c160ac47201eda5d5ae59d
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-06-13 05:51:08 +0200

    invsetup: Bump version

--- inventorysetups/inventorysetups.gradle.kts
+++ inventorysetups/inventorysetups.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.12"
+version = "0.0.13"
 
 project.extra["PluginName"] = "Inventory Setups"
 project.extra["PluginDescription"] = "Save inventory setups"
