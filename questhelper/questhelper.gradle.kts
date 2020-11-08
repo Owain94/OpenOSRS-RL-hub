4f401db4421daef641d3f272ce53eff7dcda1dd1
Owain van Brakel <owain.vanbrakel@gmail.com>
2020-11-08 22:08:38 +0100

    questhelper: bump version

--- questhelper/questhelper.gradle.kts
+++ questhelper/questhelper.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.4"
+version = "0.0.5"
 
 project.extra["PluginName"] = "Quest Helper"
 project.extra["PluginDescription"] = "An in-game interactive guide for quests"
